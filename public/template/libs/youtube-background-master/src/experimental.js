713bced059a5c18bb8ea6059c8c8a531f56cbbc6
Quang Huy Exclusive <huydaoquang@bizfly.vn>
2024-07-21 23:52:11 +0700

    update

--- public/template/libs/youtube-background-master/src/experimental.js
+++
@@ -1,7 +0,0 @@
-import { SeekBar, PlayToggle, MuteToggle, VideoBackgroundGroup, VideoBackgroundGroups } from "./lib/controls";
-
-window.SeekBar = SeekBar;
-window.PlayToggle = PlayToggle;
-window.MuteToggle = MuteToggle;
-window.VideoBackgroundGroup = VideoBackgroundGroup;
-window.VideoBackgroundGroups = VideoBackgroundGroups;
